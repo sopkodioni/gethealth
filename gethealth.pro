11b8e385364e15b874b6124d260bc3270ed315af
Denis Sopko <denis.reliable@gmail.com>
2024-03-28 01:58:25 +0200

    added page "Registration"

--- gethealth.pro
+++ gethealth.pro
@@ -10,13 +10,16 @@ CONFIG += c++17
 
 SOURCES += \
     main.cpp \
-    mainwindow.cpp
+    mainwindow.cpp \
+    registration.cpp
 
 HEADERS += \
-    mainwindow.h
+    mainwindow.h \
+    registration.h
 
 FORMS += \
-    mainwindow.ui
+    mainwindow.ui \
+    registration.ui
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
--- registration.cpp
+++ registration.cpp
@@ -1,3 +1,4 @@
+#include <QStringList>
 #include "registration.h"
 #include "ui_registration.h"
 
@@ -7,14 +8,22 @@ Registration::Registration(QWidget *parent)
 {
     ui->setupUi(this);
 
-    ui->userName->setStyleSheet("border-radius: 30px");
+    QStringList genders = (QStringList() << "Чоловіча" << "Жіноча");
 
     ui->userName->setPlaceholderText("Ім'я");
     ui->userSurname->setPlaceholderText("Прізвище");
     ui->userPatronymic->setPlaceholderText("Побатькові");
+    ui->userNumberPhone->setPlaceholderText("Номер телефону");
+    ui->userGender->setPlaceholderText("Стать");
+    ui->userPassword->setPlaceholderText("Пароль");
+    ui->userGender->addItems(genders);
+
+    ui->userNumberPhone->setValidator(new QDoubleValidator(0, 100, 2, this));
+
 }
 
 Registration::~Registration()
 {
     delete ui;
 }
+
