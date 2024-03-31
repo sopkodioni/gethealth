e6ebd905cb460d48c1af49a4797eceb40d09f037
Denis Sopko <57315214+sopkodioni@users.noreply.github.com>
2024-03-31 20:48:13 +0300

    Add files via upload

---
+++ aaa.cpp
@@ -0,0 +1,45 @@
+#include "registration.h"
+#include <QStringList>
+#include "ui_registration.h"
+
+#include "mainwindow.h"
+
+Registration::Registration(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::Registration)
+{
+    ui->setupUi(this);
+
+    QStringList genders = (QStringList() << "Чоловіча"
+                                         << "Жіноча");
+
+    ui->userName->setPlaceholderText("Ім'я");
+    ui->userSurname->setPlaceholderText("Прізвище");
+    ui->userPatronymic->setPlaceholderText("Побатькові");
+    ui->userNumberPhone->setPlaceholderText("Номер телефону");
+    ui->userGender->setPlaceholderText("Стать");
+    ui->userPassword->setPlaceholderText("Пароль");
+    ui->userGender->addItems(genders);
+
+    ui->userNumberPhone->setValidator(new QDoubleValidator(0, 100, 2, this));
+
+    connect(ui->exit, &QPushButton::clicked, this, &Registration::on_exit_clicked);
+}
+
+Registration::~Registration()
+{
+    delete ui;
+}
+
+void Registration::on_exit_clicked()
+{
+    close();
+
+    // Получаем указатель на главное окно
+    MainWindow *mainWindow = qobject_cast<MainWindow *>(parent());
+    if (mainWindow) {
+        // Показываем снова главное окно
+        mainWindow->show();
+    }
+}
+
