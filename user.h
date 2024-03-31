670315f83b38f860f9233a398e6982de07b3e888
Denis Sopko <denis.reliable@gmail.com>
2024-04-01 00:12:18 +0300

    implemented class "User"

---
+++ user.h
@@ -0,0 +1,54 @@
+#ifndef USER_H
+#define USER_H
+#include <appointment.h>
+#include <string>
+#include <vector>
+#include <QUuid>
+
+using namespace std;
+
+class User
+{
+private:
+    string id = QString(QUuid::createUuid().toByteArray()).toStdString();
+    string name;
+    string surname;
+    string patronymic;
+    string dateOfBirth;
+    string numberPhone;
+    string password;
+    string gender;
+    vector<Appointment> appointments;
+
+public:
+    User
+    (
+        string name,
+        string surname,
+        string patronymic,
+        string dateOfBirth,
+        string numberPhone,
+        string password,
+        string gender
+    );
+
+    string GetId();
+    string GetName();
+    void SetName(string name);
+    string GetSurname();
+    void SetSurname(string surname);
+    string GetPatronymic();
+    void SetPatronymic(string patronymic);
+    string GetDateOfBirth();
+    void SetDateOfBirth(string dateOfBirth);
+    string GetNumberPhone();
+    void SetNumberPhone(string numberPhone);
+    string GetPassword();
+    void SetPassword(string password);
+    string GetGender();
+    void SetGender(string gender);
+    vector<Appointment> GetAppointments();
+
+};
+
+#endif // USER_H
