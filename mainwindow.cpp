71bfd31d6993726bb03f0f0628e4c4955a8d8045
Denis Sopko <denis.reliable@gmail.com>
2024-04-03 22:23:31 +0300

    додано маркування поточного користувача

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,7 @@
 
 #include "registration.h"
 #include "personalaccount.h"
+#include "usersdata.h"
 
 #include <QFile>
 #include <QJsonArray>
@@ -22,8 +23,7 @@ MainWindow::MainWindow(QWidget *parent)
     ui->loginField->setPlaceholderText("Логін (ex: 098XXXXXXX)");
     ui->passwordField->setPlaceholderText("Пароль");
     QString phoneRegex = "^0\\d{9}$";
-    ui->loginField->setValidator(
-        new QRegularExpressionValidator(QRegularExpression(phoneRegex), this));
+    ui->loginField->setValidator(new QRegularExpressionValidator(QRegularExpression(phoneRegex), this));
     ui->loginField->setToolTip("Введіть ваш номер телефону, обов'язково починаючи з 0");
     ui->passwordField->setEchoMode(QLineEdit::Password);
 }
@@ -48,22 +48,50 @@ void MainWindow::openPersonalAccount()
 
 }
 
+
+void MainWindow::markCurrentUser(QJsonArray arrayUsersData, QJsonDocument jsonDocument, QJsonObject user, int index){
+    QJsonObject currentUser = user;
+
+    currentUser["current"] = "true";
+
+    arrayUsersData[index] = currentUser;
+
+    jsonDocument.setArray(arrayUsersData);
+
+    QFile usersList("users.json");
+    if (!usersList.open(QIODevice::WriteOnly)) {
+        qDebug() << "Failed to open output file";
+    }
+    usersList.write(jsonDocument.toJson());
+    usersList.close();
+}
+
 void MainWindow::checkAuthorization(const QString &phoneNumber, const QString &password)
 {
-    UsersData usersData;
+    QFile jsonUsers("users.json");
+    if (!jsonUsers.open(QIODevice::ReadOnly)){
+        qWarning("Couldn't open file.");
+    }
+    QByteArray usersData = jsonUsers.readAll();
+    QJsonDocument jsonDocument = QJsonDocument::fromJson(usersData);
 
-    foreach (const QJsonValue &value, usersData.getArrayUsersData()) {
-        QJsonObject user = value.toObject();
-        QString userPhoneNumber = user.value("phoneNumber").toString().trimmed();
-        QString userPassword = user.value("password").toString().trimmed();
-        if (userPhoneNumber == phoneNumber.trimmed()) {
-            if (userPassword == password.trimmed()) {
+    QJsonArray arrayUsersData = jsonDocument.array();
+
+    for(int i = 0; i < arrayUsersData.size(); i++){
+        QJsonObject user = arrayUsersData[i].toObject();
+
+        QString inputPhoneNumber = user.value("phoneNumber").toString().trimmed();
+        QString inputPassword = user.value("password").toString().trimmed();
+
+        if (inputPhoneNumber == phoneNumber.trimmed()) {
+            if (inputPassword == password.trimmed()) {
+                this->markCurrentUser(arrayUsersData, jsonDocument, user, i);
                 openPersonalAccount();
                 return;
-            } else {
-                QMessageBox::critical(nullptr, "Error", "Невірний пароль");
-                return;
             }
+
+            QMessageBox::critical(nullptr, "Error", "Невірний пароль");
+            return;
         }
     }
 
@@ -126,3 +154,5 @@ void MainWindow::on_comeInButton_clicked()
 
     checkAuthorization(phoneNumber, password);
 }
+
+
